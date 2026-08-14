cb4a0f96258e45b26567db6637446d1a558e2a49
Aqualavatic <duongnamhai52@gmail.com>
2026-08-14 14:04:53 +0700

    Delete Backend directory

--- Backend/download_data.py
+++
@@ -1,8 +0,0 @@
-from roboflow import Roboflow
-
-# Tải trực tiếp bộ dữ liệu TACO được format chuẩn YOLOv8
-rf = Roboflow(api_key="ANONYMOUS") # Hoặc tạo tài khoản Roboflow miễn phí để lấy API Key
-project = rf.workspace("vrb").project("taco-trash-annotations-in-context")
-dataset = project.version(1).download("yolov8")
-
-print("✅ Đã tải xong dataset về thư mục:", dataset.location)
